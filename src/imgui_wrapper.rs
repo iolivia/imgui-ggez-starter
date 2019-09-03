ffe228cb3eef9b92f4b840d0347612348712f735
Olivia <olivia_github@outlook.com>
2019-09-03 08:00:35 +0100

    Properly calculate display scale

--- src/imgui_wrapper.rs
+++ src/imgui_wrapper.rs
@@ -72,9 +72,12 @@ impl ImGuiWrapper {
     let delta_s = delta.as_secs() as f32 + delta.subsec_nanos() as f32 / 1_000_000_000.0;
     self.last_frame = now;
 
-    let (w, h) = graphics::drawable_size(ctx);
-    self.imgui.io_mut().display_size = [w, h];
-    self.imgui.io_mut().display_framebuffer_scale = [2.0, 2.0];
+    let (width, height) = graphics::size(ctx);
+    let (draw_width, draw_height) = graphics::drawable_size(ctx);
+    let scale_x = (draw_width as f32) / (width as f32);
+    let scale_y = (draw_height as f32) / (height as f32);
+    self.imgui.io_mut().display_size = [width, height];
+    self.imgui.io_mut().display_framebuffer_scale = [scale_x, scale_y];
     self.imgui.io_mut().delta_time = delta_s;
 
     let ui = self.imgui.frame();
